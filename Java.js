4a61aca186bc5e32b66026e9ba2f3719c59e34eb
Juhriber <j.de.s.r.25@gmail.com>
2021-10-02 18:46:20 -0300

    NomeCompletoJS

---
+++ Java.js
@@ -0,0 +1,12 @@
+let Nome = prompt ("Seu primeiro nome por gentileza: ");
+
+let Sobrenome = prompt ("Agora seu sobrenome: ");
+
+let NomeCompleto = `Seu Nome é ? : ${Nome} ${Sobrenome}.`
+
+alert (NomeCompleto);
+
+alert("Olá Bem Vindo ao GitHub")
+
+alert("Agora uma Frase para refletir!");
+alert("“Por maior que seja, não há obstáculo que não possa ser superado”!");
--- README.md
+++
@@ -1,2 +0,0 @@
-# NomeCompletoJs
-Reimplementação em JavaScript do exercício NomeCompleto da lista "Entrada de dados e strings" do Dev C#: NomeCompleto
